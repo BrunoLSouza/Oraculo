1564260211efec2e6835aafd602a3441e885f131
Bruno Pinheiro Lemes de Souza <bruno.lemes@fgv.br>
2019-08-12 13:42:08 -0300

    Implementação repositorio redis Add chamandas do repositorio na main

--- Oraculo/Oraculo/Program.cs
+++ Oraculo/Oraculo/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 
 namespace Oraculo
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Oraculo em execução !!!");
+            Console.WriteLine("Serviço online!");
+            var redis = RedisRepositorio.getConnection().GetSubscriber();
+            var db = RedisRepositorio.GetDb();
+            redis.Subscribe("Perguntas", (ch, msg) =>
+            {
+                var array = msg.ToString().Split(":");
+                db.HashSet(array[0], new HashEntry[] { new HashEntry("Equipe 1-2-3", "Palmeiras não tem mundial") });
+
+                Console.WriteLine($"pergunta : {array[0]}, Hash: Equipe 1-2-3 | Palmeiras não tem mundial");
+            });
+            Console.ReadKey();
         }
     }
 }
---
+++ Oraculo/Oraculo/RedisRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oraculo
+{
+    public class RedisRepositorio
+    {
+        private static ConnectionMultiplexer _connection { get; set; }
+        public static IDatabase GetDb()
+        {
+            var redis = _connection;
+            return redis.GetDatabase();
+        }
+
+        public static ConnectionMultiplexer getConnection()
+        {
+            if (_connection is null)
+                _connection = ConnectionMultiplexer.Connect("40.77.24.62");
+            return _connection;
+        }
+    }
+}
